7898de213bf18074ca548640f0338551eb9ce9bf
Tyroneekhator <72547969+Tyroneekhator@users.noreply.github.com>
2026-06-25 16:30:27 +0100

    better project

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,3 +1,12 @@
-from app.models.product import Product
+from app.models.admin_user import AdminUser
 from app.models.chat_message import ChatMessage
 from app.models.order import Order, OrderItem
+from app.models.product import Product
+
+__all__ = [
+    "AdminUser",
+    "ChatMessage",
+    "Order",
+    "OrderItem",
+    "Product",
+]
